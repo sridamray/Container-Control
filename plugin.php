3cac4f4ff2a794aa6b99670af76458015d336972
Sridam Ray <sridamroybd@gmail.com>
2024-08-15 23:53:15 +0600

    common Colum Section container Control add hooks here

--- plugin.php
+++ plugin.php
@@ -15,8 +15,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 use Elementor\Controls_Manager;
 class CustomContainerWidget {
 
+
     public function __construct() {
+        add_action( 'elementor/element/common/_section_style/after_section_end', [ $this, 'register_controls' ] );
+        add_action( 'elementor/element/column/section_advanced/after_section_end', [ $this, 'register_controls' ] );
+        add_action( 'elementor/element/section/section_advanced/after_section_end', [ $this, 'register_controls' 
+ ] );
         add_action( 'elementor/element/container/section_layout/after_section_end', [ $this, 'register_controls' ] );
+        add_action( 'elementor/frontend/before_render', [ $this, 'before_render' ], 1 );
     }
 
     public function register_controls( $element ) {
